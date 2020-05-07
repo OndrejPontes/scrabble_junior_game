a8084a60da9b4a1c08eb6e81bb1c5d7cec684b05
Martin Majtan <majtan.mato@gmail.com>
2020-05-07 19:42:32 +0200

    drawTiles(), removeTilesFromPool() added

--- ScrabbleJunior/ScrabbleJunior.cpp
+++ ScrabbleJunior/ScrabbleJunior.cpp
@@ -7,20 +7,44 @@ using namespace std;
 #include <string>
 #include <vector>
 #include <cstdlib>
+#include <ctime>
 
 void Game::startGame() {
 	
-
+// get # of players, get names and print them
 	getNumberOfPlayers();
 	for (int i = 0; i < numberOfPlayers; i++) {
 		Player newPlayer= Player();
 		newPlayer.getName();
 		players.push_back(newPlayer);
 	}
+	cout << "Our players are: ";
 	for (int j = 0; j < players.size(); j++) {
-		cout << players[j].name;
+		cout << players[j].name<<" ";
 	}
-	cout<<board.scoringChips;
+
+// set pool and print it
+	cout << "\n POOL BEFORE DRAW: \n";
+
+	board.setPool(board.pool);
+	cout <<"\n" <<board.pool.size()<<"\n";
+	for (int k = 0; k < board.pool.size(); k++)
+		cout << board.pool[k] << " ";
+
+// draw tiles and print
+	cout << "\n PLAYER'S TILES: \n";
+	vector<int> toRemove= players[0].drawTiles(board.pool);
+	for(int temp=0;temp<players[0].playersPool.size();temp++)
+		cout << players[0].playersPool[temp] << " ";
+
+// remove tiles, which were draw from pool
+	board.removeTilesFromPool(toRemove);
+
+// print pool after draw
+	cout <<"\n"<< board.pool.size()<<endl;
+	cout << "\n POOL AFTER DRAW: \n";
+	for(int t=0;t<board.pool.size();t++)
+		cout << board.pool[t] << " ";
 }
 
 
@@ -40,6 +64,7 @@ int Game::getNumberOfPlayers() {
 			numberOfPlayers = input;
 			keepGoing = false;
 		}
+
 		
 	}
 
@@ -58,4 +83,38 @@ void Player::getName() {
 
 Board::Board() {
 	scoringChips = 44;
+}
+
+void Board::setPool(vector<char> &pool) {
+    int numOfTiles = 101;
+	char letter;
+
+	srand(time(NULL));
+	for (int i = 0; i < numOfTiles; i++)
+	{
+		letter = 'a' + rand() % 26;            // Convert to a character from a-z
+		pool.push_back(letter);
+	}
+
+}
+
+vector<int> Player::drawTiles(std::vector<char> pool){
+	int r, numOfPlayersTile = 7;
+	vector<int> toRemove;
+	srand(time(NULL));
+	for (int i = 0; i < numOfPlayersTile; i++) {
+		r = rand() % pool.size();
+		playersPool.push_back(pool[r]);
+		toRemove.push_back(r);
+		cout << r << " ";
+	}
+
+	return toRemove;
+}
+
+void Board::removeTilesFromPool(vector<int> toRemove) {
+	for(int i=0;i<toRemove.size();i++){
+		pool.erase(pool.begin() + toRemove[i]);
+	}
+
 }
--- ScrabbleJunior/ScrabbleJunior.h
+++ ScrabbleJunior/ScrabbleJunior.h
@@ -12,8 +12,8 @@ public:
     int score;
 
     Player();
-    std::vector<char> drawTiles();                     // draw tiles from pool
-    std::vector<char> updateTiles();                   // check if player has 7 tiles,after his turn it will update #ofHisTiles
+    std::vector<int> drawTiles(std::vector<char> pool);          // draw tiles from pool and returns vec<int> with indexs of tiles
+    std::vector<char> updateTiles();                             // check if player has 7 tiles,after his turn it will update #ofHisTiles
     std::vector<char> playTurn();                                // choose  2 tiles to play and position
                                                                  // check for invalid tiles (if player has choosen tiles in hand)
     int  updateScore();                                                                                            
@@ -28,15 +28,16 @@ public:
 // class attributes of board e.g. pool, scoring chips
 class Board {
 public:
-     const std::vector<char> pool;                  // with 101  tiles
+     std::vector<char> pool;                  // with 101  tiles
       int scoringChips;                              // 44 chips
+    
 
 //private:
       
 //public:
     Board();
-    std::vector<char> removeTilesFromPool();        // when player calls functions  'drawTiles' or 'updateTiles' that tiles must be removed from pool
-    void setPool();
+    void removeTilesFromPool(std::vector<int> toRemove);   // when player calls functions  'drawTiles' or 'updateTiles' that tiles must be removed from pool
+    void setPool(std::vector<char> &pool);
 
 };
 
@@ -46,6 +47,7 @@ public:
     int numberOfPlayers;
     std::vector<Player> players;
     Board board;
+
 
     int getNumberOfPlayers();
     void printBoard();                                 // print board from board file.txt
