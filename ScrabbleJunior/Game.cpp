53e4dd93981cd1eacc2c5c5f0d984b08c8ca3ddd
Ondrej Ponteš <up201911215@fe.up.pt>
2020-05-14 23:17:19 +0200

    Added getLetters to Player

--- ScrabbleJunior/Game.cpp
+++ ScrabbleJunior/Game.cpp
@@ -3,13 +3,10 @@
 using namespace std;
 
 #include <iostream>
-#include <fstream>
 #include <sstream>
 #include <iterator>
 #include <string>
 #include <vector>
-#include <cstdlib>
-#include <ctime>
 #include <algorithm>
 #include <random>
 
@@ -141,114 +138,38 @@ bool Game::gameDoesntHaveWinner() {
     return totalScore < board.getWordCount() && pool.size() != 0;
 }
 
-// Od tialto som s tym nic nerobil
-//
-//void Game::coverTiles() {
-//    int posX, posY;
-//    vector<string> parsed;
-//    string line;
-//    bool keepGoing = true;
-//    cout
-//            << "Enter tile that you want to cover in format  'Ak T'. The first pair represents the coordinates where you want to place the tile."
-//               "Second parameter is your tile." << endl;
-//    cin.ignore();
-//    // while user's input is not correct, user must enter tile again
-//    while (keepGoing) {
-//        cout << "\nYour tile: ";
-//
-//        getline(cin, line);
-//        istringstream iss(line);
-//        parsed = {
-//                istream_iterator<string>(iss), {}
-//        };
-//        posX = (int) parsed[0][0] - 64;
-//        posY = (int) parsed[0][1] - 96;
-//        if ((parsed.size() == 2) && (parsed[0].size() == 2) && (posX >= 1) && (posX <= board.getDimensionX()) &&
-//            (posY >= 1) && (posY <= board.getDimensionY())) {
-//
-//            if (parsed[1][0] != board.getTile((int) parsed[0][0], (int) parsed[0][1]) &&
-//                find(players[0].letters.begin(), players[0].letters.end(), parsed[1][0])
-//                == players[0].letters.end()) {
-//                cout << "You can't cover this tile!";
-//            } else {
-//                board.takeTile((int) parsed[0][0], (int) parsed[0][1]);
-//                keepGoing = false;
-//            }
-//        } else {
-//            cout << " Enter excatly 2 parameters and check if parameters  do not exceeds dimensions of board! ";
-//        }
-//
-//    }
-//}
-//
-//void Game::drawTiles() {
-//    int r, numOfPlayersTile = 7;
-//
-//    srand(time(NULL));
-//    for (int i = 0; i < players.size(); i++) {
-//        for (int j = 0; j < numOfPlayersTile; j++) {
-//            r = rand() % pool.size();
-//            players[i].letters.push_back(popLetter(r));
-//        }
-//    }
-//    showPlayersTiles();
-//
-//
-//}
-//
-//// Initialization of random letters in the pool
-//Game::Game() {
-//    getSettings();
-//    char letter;
-//    srand(time(NULL));                            // to make initialization random using time seed
-//    for (int i = 0; i < numOfTiles; i++) {
-//        letter = 'a' + rand() % 26;            // Convert to a character from a-z
-//        pool.push_back(letter);
-//    }
-//}
-//
-//void Game::getSettings() {// initialization parameters of game
-//    int input;
-//    bool keepGoing = true;
-//    cout << "\t\tWELCOME TO SCRABBLE GAME !\nFirst of all, let's go through the game settings.\n\n";
-//    while (keepGoing) {
-//        cout << "How many tiles do you want to play with(Minimum value for two players is 14): ";
-//        cin >> input;
-//        if (cin.fail()) {
-//            cin.clear();
-//            cin.ignore(256, '\n');
-//            cout << "Enter valid value!\n";
-//        } else if (input >= 14) {
-//            keepGoing = false;
-//            numOfTiles = input;
-//        }
-//    }
-//
-//}
-//
-//void Game::showPool() {
-//    string input;
-//    // with 101 letters in pool, it might be unclear in terminal
-//    cout << "\nIf you want to see pool, press 'y': ";
-//    cin >> input;
-//    if (input == "y") {
-//        cout << "OUR POOL IS: \n";
-//        for (int k = 0; k < pool.size(); k++)
-//            cout << pool[k] << " ";
-//        cout << "\nNumber of letters in pool: " << pool.size() << "\n";
-//
-//    }
-//
-//}
-//
-//void Game::showPlayersTiles() {
-//    cout << endl << " PLAYER'S TILES: " << endl;
-//    for (int temp = 0; temp < players.size(); temp++) {
-//        cout << "Player " << temp << ".: ";
-//        for (int l = 0; l < players[temp].letters.size(); l++) {
-//            cout << players[temp].letters[l] << " ";
-//
-//        }
-//        cout << endl;
-//    }
-//}
+void Game::coverTiles() {
+    int posX, posY;
+    vector<string> parsed;
+    string line;
+    bool keepGoing = true;
+    cout << "Enter tile that you want to cover in format  'Ak T'. The first pair represents the coordinates where you "
+            "want to place the tile. Second parameter is your tile." << endl;
+
+    // while user's input is not correct, user must enter tile again
+    while (keepGoing) {
+        cout << endl << "Your tile: ";
+
+        getline(cin, line);
+        istringstream iss(line);
+        parsed = {
+                istream_iterator<string>(iss), {}
+        };
+        posX = (int) parsed[0][0] - 64;
+        posY = (int) parsed[0][1] - 96;
+        if ((parsed.size() == 2) && (parsed[0].size() == 2) && (posX >= 1) && (posX <= board.getDimensionX()) &&
+            (posY >= 1) && (posY <= board.getDimensionY())) {
+
+            if (parsed[1][0] != board.getTile((int) parsed[0][0], (int) parsed[0][1]) &&
+                find(players[0].getLetters().begin(), players[0].getLetters().end(), parsed[1][0])
+                == players[0].getLetters().end()) {
+                cout << "You can't cover this tile!";
+            } else {
+                board.takeTile((int) parsed[0][0], (int) parsed[0][1]);
+                keepGoing = false;
+            }
+        } else {
+            cout << " Enter excatly 2 parameters and check if parameters  do not exceeds dimensions of board! ";
+        }
+    }
+}
